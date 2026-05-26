7d5691a2ddc128419d8c1616fb413c2c12a15d35
Bogdan-Danilovic <78044400+Bogdan-Danilovic@users.noreply.github.com>
2026-05-26 10:52:14 +0200

    Add Impostor layout with dark background (#080b14) and breathing orbs

---
+++ app/games/impostor/layout.tsx
@@ -0,0 +1,14 @@
+export default function ImpostorLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="relative min-h-full bg-[#080b14] text-[#c8d0e0]">
+      <div className="breathing-orb w-[500px] h-[500px] bg-violet-600/30 -top-40 -left-40" />
+      <div className="breathing-orb w-[400px] h-[400px] bg-fuchsia-600/20 -bottom-32 -right-32" style={{ animationDelay: '4s' }} />
+      <div className="scanline" />
+      {children}
+    </div>
+  );
+}
